94969050768401d41873a2d3be8d752186bf8b49
Arthur Meier <Arthurcmeier@outlook.com>
2023-04-03 17:27:45 -0300

    Entrega parcialmente corregida

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,13 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { market } from "./database";
+import { IProduct } from "./interfaces";
 
 const verifyNameExist = (req: Request, res: Response, next: NextFunction) => {
-  const { name } = req.body;
+  if (req.body && Array.isArray(req.body)) {
+    req.body.forEach((item: IProduct) => {
+      const { name } = item;
 
-  const product = market.find((p) => p.name === name);
-  if (!!product) {
-    return res.status(409).json({ error: "Product name already exists" });
+      const product = market.find((p) => p.name === name);
+
+      if (!!product) {
+        return res.status(409).json({ error: "Product name already exists" });
+      }
+    });
+  } else {
+    const { name } = req.body;
+
+    const product = market.find((p) => p.name === name);
+
+    if (!!product) {
+      return res.status(409).json({ error: "Product name already exists" });
+    }
   }
+
   next();
 };
 
